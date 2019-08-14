f7e519a2620db8a1021734cdef1b4fdd688c7fc0
MikeK <mgkittridge@gmail.com>
2019-08-14 16:27:25 +1200

    update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ version = '0.0.1'
 if os.environ.get('READTHEDOCS', False) == 'True':
     INSTALL_REQUIRES = []
 else:
-    INSTALL_REQUIRES = ['pandas', 'pdsql', 'hydrointerp']
+    INSTALL_REQUIRES = ['pandas', 'pdsql', 'hydrointerp', 'yaml']
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
